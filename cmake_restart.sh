c967474c2f05eb814f93067aa2d9d756a016d8ef
Loren Oh <loh@eoas.ubc.ca>
2018-07-23 19:19:57 -0400

    Exit when SAM_DIR not found

--- cmake_restart.sh
+++ cmake_restart.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-./cmake_config.sh
-
-set -e
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+. $dir/cmake_config.sh
 
 if [ -z "$SAM" ]; then
 	echo "SAM ENV VAR NOT FOUND"
+	exit 1
 else
 	# Check if leftover files exist
-	cache=( "CMakeFiles" "lib" "UTIL" "CMakeCache.txt" "include_fortran" "Makefile" "SAM_CMAKE" "cmake_install.cmake" )
+	cache=( "CMakeFiles" 
+		"lib" 
+		"UTIL" 
+		"CMakeCache.txt" 
+		"include_fortran" 
+		"Makefile" 
+		"SAM_CMAKE" 
+		"cmake_install.cmake" )
 	for file in "${cache[@]}"
 	do
 		if [ -e $file ]; then 
