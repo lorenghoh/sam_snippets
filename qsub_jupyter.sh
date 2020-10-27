99a676064a125c2a4ee83e912cadeae6147c9e69
Loren Oh <loh@eoas.ubc.ca>
2020-10-27 06:32:55 -0400

    Use ntasks-per-node

--- qsub_jupyter.sh
+++ qsub_jupyter.sh
@@ -1,2 +1,2 @@
 source ~/pyenv/stats/bin/activate
-salloc --nodes=1 --ntasks=1 --tasks-per-node=32 --mem=0 --time=3:00:00 --account=def-paustin srun $VIRTUAL_ENV/bin/notebook.sh
+salloc --nodes=1 --ntasks-per-node=32 --mem=0 --time=3:00:00 --account=def-paustin srun $VIRTUAL_ENV/bin/notebook.sh
